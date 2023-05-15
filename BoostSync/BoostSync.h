62a5eddc3b886d1b99327d534a88cea446613b60
Kwonyoungoh <rnjsduddh05@gmail.com>
2023-05-15 16:03:33 +0900

    0515#2

---
+++ BoostSync/BoostSync.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <hiredis/hiredis.h>
+#include <nlohmann/json.hpp>
+
+using namespace std;
+using json = nlohmann::json;
+using boost::asio::ip::udp;
